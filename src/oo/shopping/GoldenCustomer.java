9f46c5f9e79795ec9d755e30fd5b10cc36885ffe
Hank Tom <hanktom@gmail.com>
2017-05-09 16:31:16 +0800

    Inheritance step 1

---
+++ src/oo/shopping/GoldenCustomer.java
@@ -0,0 +1,5 @@
+package oo.shopping;
+
+public class GoldenCustomer extends SilverCustomer {
+	float returnRate = 0.05f;
+}
---
+++ src/oo/shopping/SilverCustomer.java
@@ -0,0 +1,6 @@
+package oo.shopping;
+
+public class SilverCustomer extends Customer {
+	float discount = 0.1f;
+	
+}
