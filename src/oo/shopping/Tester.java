1f71c1bf3fd2704f5e5e95c16532d2b4ae7ec943
user <user@es302nt>
2017-05-16 16:24:30 +0800

    for each and 轉型

--- src/oo/shopping/Tester.java
+++ src/oo/shopping/Tester.java
@@ -13,13 +13,24 @@ public class Tester {
 		list.add(new GoldenCustomer(15000));
 		list.add(new SilverCustomer(1000));
 		list.add(new Customer(6000));
-		for (int i=0; i<list.size(); i++){
-			Customer cust = list.get(i);
+//		for (int i=0; i<list.size(); i++){
+//			Customer cust = list.get(i);
+		for (Customer cust : list){
 			if (cust instanceof SilverCustomer &&
 					!(cust instanceof GoldenCustomer)){
 				System.out.print("*");
+				SilverCustomer silver = (SilverCustomer)cust;
 			}
 			cust.print();
+		}
+		
+		String[] array = {"aa","bb","cc"};
+		for (String s : array){
+			System.out.println(s);
+		}
+		for (int i=0; i<array.length; i++){
+			String s = array[i];
+			System.out.println(s);
 		}
 	}
 }
