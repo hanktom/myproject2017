34d5e008a54ac4b1fbebc5f43eb8f1d5ffb8d714
Hank Tom <hanktom@gmail.com>
2017-06-13 16:07:55 +0800

    sub-class constructors

--- src/oo/test/PhoneMember.java
+++ src/oo/test/PhoneMember.java
@@ -3,6 +3,12 @@ package oo.test;
 public class PhoneMember extends Member{
 	String phone;
 	boolean verified = false;
+	public PhoneMember(String name){
+		super(name);
+	}
+	public PhoneMember(){
+		super();
+	}
 	public String getPhone() {
 		return phone;
 	}
